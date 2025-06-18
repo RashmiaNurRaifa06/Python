c30bf4713e3977a544359be7a394a0969e42669e
RashmiaNurRaifa06 <rashedul.bb2009@gmail.com>
2025-06-18 17:47:45 +0600

    Add files via upload

--- 1011_pygameintro.py
+++ 1011_pygameintro.py
@@ -3,11 +3,6 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((500,500))
-
-font = pygame.font.SysFont(None,48)
-text = font.render('Codingal', True, (255,255,255))
-text_rect = text.get_rect(center = (250,250))
-
 done = False
 
 while not done:
@@ -15,8 +10,6 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
             
-    screen.fill((0,0,0))
-    screen.blit(text,text_rect)
-    pygame.display.flip()
+    pygame.draw.rect(screen, (0,125,255), pygame.Rect(38,38,200,130))
     
-pygame.quit()
+    pygame.display.flip()
