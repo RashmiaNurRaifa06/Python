a55fd2e266e99c3b414e11f17b40e5fde7c5f496
RashmiaNurRaifa06 <rashedul.bb2009@gmail.com>
2025-07-14 17:30:28 +0600

    Add files via upload

---
+++ 1024_loginapp.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import messagebox
+
+VALID_USERNAME = "Rashmia"
+VALID_PASSWORD = "06122014"
+
+def login():
+    username = username_entry.get()
+    password = password_entry.get()
+    
+    if username == VALID_USERNAME and password == VALID_PASSWORD:
+        messagebox.showinfo("Login succesful",f"Welcome, {username}!")
+    else:
+        messagebox.showerror("Login failed.", "Invalid username or password, please try again.")
+        
+root = tk.Tk()
+root.title("Login Application")
+root.geometry("300x200")
+
+tk.Label(root, text = "Username: ").grid(row = 0, column = 0, padx = 10, pady = 10, sticky = "w")
+username_entry = tk.Entry(root, width = 25)
+username_entry.grid(row = 0, column = 1)
+
+tk.Label(root, text = "Password: ").grid(row = 1, column = 0, padx = 10, pady = 10, sticky = "w")
+password_entry = tk.Entry(root, width = 25, show = '*')
+password_entry.grid(row = 1, column = 1)
+
+login_button = tk.Button(root, text = "Login", width = 15, command = login)
+login_button.grid(row = 2, column = 1, pady = 20)
+
+root.mainloop()
