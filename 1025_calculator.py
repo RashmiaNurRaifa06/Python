843307acc32968ab24c6833d9d82f0b8f98cfea6
RashmiaNurRaifa06 <rashedul.bb2009@gmail.com>
2025-07-16 17:19:30 +0600

    Add files via upload

--- 1025_calculator.py
+++ 1025_calculator.py
@@ -17,7 +17,7 @@ def click(event):
         
 root = tk.Tk()
 root.title("Calculator")
-root.geometry("400x500")
+root.geometry("400x600")
 root.resizable(0, 0)
 root.configure(bg = "#222831")
 
@@ -36,5 +36,30 @@ btn_texts = [
 ]
 
 btn_colors = [
-    ["#00adb5"]
+    ["#00adb5", "#00adb5", "#00adb5", "#ff5722"],
+    ["#00adb5", "#00adb5", "#00adb5", "#ff5722"],
+    ["#00adb5", "#00adb5", "#00adb5", "#ff5722"],
+    ["#00adb5", "#00adb5", "#f44336", "#ff5722"],
+    ["#4caf50"]
 ]
+
+for i, row in enumerate(btn_texts):
+    frame = tk.Frame(btns_frame, bg = "#222831")
+    frame.pack(expand = True, fill = "both")
+    for j, text in enumerate(row):
+        btn = tk.Button(
+            frame,
+            text = text,
+            font = "Arial 20 bold",
+            width = 5,
+            height = 2,
+            bg = btn_colors[i][j],
+            fg = "#eeeeee",
+            activebackground = "#393e46",
+            activeforeground = "#00adb5",
+            borderwidth = 0   
+        )
+        btn.pack(side = "left", expand = True, fill = "both", padx = 5, pady = 5)
+        btn.bind("<Button - 1>", click)
+        
+root.mainloop()
